554d4f779f311e3be0407bef0a3e7465948f6864
Prashanga <prashanga@gmail.com>
2020-09-07 19:37:58 +1000

    First commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import Navbar from 'components/Navbar.vue'
+
+
+export default {
+  name: 'MainLayout',
+  components: {  },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
