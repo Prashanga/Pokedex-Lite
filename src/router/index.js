fe3c0e04e999da646aadfd1f7d978cccd25354c0
Prashanga <prashanga@gmail.com>
2020-10-02 21:50:10 +1000

    Enabling History Mode

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,7 @@ export default function (/* { store, ssrContext } */) {
     // Leave these as they are and change in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
-   // mode: 'history',
+    mode: 'history',
     base: process.env.VUE_ROUTER_BASE
   })
 
