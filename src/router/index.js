615071378d379d87b17c4e4c760ab6d04d6fe05f
Prashanga <prashanga@gmail.com>
2020-10-03 10:09:19 +1000

    Saving scroll position in the main page

--- src/router/index.js
+++ src/router/index.js
@@ -16,20 +16,28 @@ Vue.use(VueRouter)
 
 export default function (/* { store, ssrContext } */) {
   const Router = new VueRouter({
-    scrollBehavior: () => ({ x: 0, y: 0 }),
     routes,
 
     // Leave these as they are and change in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
     mode: 'history',
-    base: process.env.VUE_ROUTER_BASE
+    base: process.env.VUE_ROUTER_BASE,
+    scrollBehavior (to, from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition
+      }
+       else {
+        return { x: 0, y: 0 }
+      }
+    }
+
   })
 
   Router.beforeEach((to, from, next) => {
     document.title = to.meta.title
     next()
-})
+  })
 
   return Router
 }
